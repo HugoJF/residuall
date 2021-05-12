6b3c0f3a84d8598f652baf38c6ab8f0c0750bc29
Hugo Jeller <hugo_jeller@hotmail.com>
2021-05-12 18:56:06 -0400

    Parte 2

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send({
-        status: "OK",
+app.use(express.json());
+
+function ok(results = []) {
+    return {
+        status: 'OK',
         code: 200,
-        results: []
-    });
+        results
+    }
+}
+
+function isValidEmail(email) {
+    if (typeof email !== 'string') {
+        return false;
+    }
+
+    // [1 ou mais caracteres] @ [1 ou mais caracteres] [1 das 4 opções] [fim da string]
+    return email.match(/.+@.+(.com.br|.com|.gov.br|.org)$/) !== null;
+}
+
+app.get('/', function (req, res) {
+    res.send(ok());
 });
 
 app.get('/health', function (req, res) {
-    res.send({
-        status: "OK",
-        code: 200,
-        results: [{
-            message: "Servidor executando na porta 8080"
-        }]
-    });
+    res.send(ok([{
+        message: 'Servidor executando na porta 8080'
+    }]));
+});
+
+app.post('/mail/validation/v1', function (req, res) {
+    let emails = req.body.email_address;
+
+    if (!Array.isArray(emails)) {
+        emails = [emails];
+    }
+
+    res.send(ok(
+        emails.map(email => ({
+            'email_address': email,
+            'domain': 'mail',
+            'valid_syntax': isValidEmail(email),
+        }))
+    ));
 });
 
 app.listen(8080);
